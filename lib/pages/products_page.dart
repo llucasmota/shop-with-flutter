c40d83d0d52e93ee3cc9bee92ee01bbadfa7e836
llucasmota <lucas.o.mota@gmail.com>
2023-02-19 17:59:28 -0300

    feat(products_page): adicionando página de gerenciamento de produtos

---
+++ lib/pages/products_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/utils/app_routes.dart';
+
+class ProductsPage extends StatelessWidget {
+  const ProductsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ProductList productList = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gerenciar Produtos'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PRODUCT_FORM);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+            itemCount: productList.itemsCount,
+            itemBuilder: (ctx, index) {
+              return ProductItem(product: productList.items[index]);
+            }),
+      ),
+    );
+  }
+}
