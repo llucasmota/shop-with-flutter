d242e9d8bdd2a41c9e6f7e5889c800d00c81d96e
llucasmota <lucas.o.mota@gmail.com>
2023-03-01 20:56:37 -0300

    feat(product_list): adicionando o método saveProduct e o método updateProduct

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
@@ -12,6 +14,34 @@ class ProductList with ChangeNotifier {
 
   List<Product> get favoriteItems =>
       _items.where((product) => product.isFavorite).toList();
+
+  void saveProduct(Map<String, Object> data) {
+    bool hasId = data['id'] != null;
+
+    final product = Product(
+        id: hasId ? data['id'] as String : Random().nextDouble().toString(),
+        name: data['name'] as String,
+        description: data['description'] as String,
+        price: data['price'] as double,
+        imageUrl: data['imageUrl'] as String);
+
+    if (hasId) {
+      updateProduct(product);
+    } else {
+      addProduct(product);
+    }
+  }
+
+  void updateProduct(Product product) {
+    // tem um indice válido?
+
+    int index = _items.indexWhere((prd) => prd.id == product.id);
+
+    if (index >= 0) {
+      _items[index] = product;
+      super.notifyListeners();
+    }
+  }
 
   void addProduct(Product product) {
     _items.add(product);
