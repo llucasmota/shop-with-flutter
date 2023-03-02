838922bb6b7c0bec828924cb4fa4199b8d56035d
llucasmota <lucas.o.mota@gmail.com>
2023-03-01 21:49:42 -0300

    feat(product_item): adicionando dialog ao tentar excluir, bem como sackBar

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -30,7 +32,52 @@ class ProductItem extends StatelessWidget {
               },
             ),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  showDialog<bool>(
+                    context: context,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: Text(
+                          'Exclusão',
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary),
+                        ),
+                        content: const Text('Deseja realmente excluir?'),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.of(ctx).pop(false),
+                              child: const Text('Não')),
+                          TextButton(
+                              onPressed: () => Navigator.of(ctx).pop(true),
+                              child: const Text('Sim'))
+                        ],
+                      );
+                    },
+                  ).then((value) {
+                    if (value ?? false) {
+                      Provider.of<ProductList>(context, listen: false)
+                          .removeProductByProductId(product.id);
+                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text(
+                          'Produto excluído com sucesso.',
+                          style: TextStyle(color: Colors.greenAccent),
+                        ),
+                        duration: Duration(seconds: 2),
+                      ));
+                    }
+                  });
+
+                  // Provider.of<ProductList>(context, listen: false)
+                  //     .removeProductByProductId(product.id);
+
+                  // ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  // ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  //   content: Text('Produto excluído com sucesso'),
+                  //   duration: Duration(seconds: 2),
+                  // ));
+                  // Navigator.of(context).pushNamed(AppRoutes.HOME);
+                },
                 icon: Icon(Icons.delete,
                     color: Theme.of(context).colorScheme.error)),
           ],
