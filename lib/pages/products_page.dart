04cc6a18e306ea897e5fac45ae0c0049f4c71f73
llucasmota <lucas.o.mota@gmail.com>
2023-04-10 07:31:10 -0300

    feat(products_page): adicionado consumo do provider Auth para enviar userId ao loadProducts

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/product_item.dart';
+import 'package:shop/models/auth.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -13,7 +14,7 @@ class ProductsPage extends StatelessWidget {
     return Provider.of<ProductList>(
       context,
       listen: false,
-    ).loadProducts();
+    ).loadProducts(Provider.of<Auth>(context, listen: false).userId ?? '');
   }
 
   @override
